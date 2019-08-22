75e584594c41b47720a35633badbd6388ad2e8e1
Corleone <corleonexiao@qq.com>
2019-08-22 13:29:02 +0800

    添加了travis浏览器启动配置

--- front/build/webpack.test.config.js
+++ front/build/webpack.test.config.js
@@ -4,15 +4,14 @@ const webpack = require('webpack');
 const AddHtmlAssets = require('add-asset-html-webpack-plugin');
 
 const config = merge(baseConfig, {
-    mode: 'none',
-    devtool: '#inline-source-map',
+    mode: 'development',
+    devtool:'eval',
     plugins: [
         new webpack.DefinePlugin({
-            'progress.env.NODE_ENV': JSON.stringify('test'),
+            'progress.env.BABEL_ENV': JSON.stringify('test'),
         }),
     ],
 });
 
 config.plugins = config.plugins.filter(item => !(item instanceof webpack.ProgressPlugin || item instanceof webpack.DllReferencePlugin || item instanceof AddHtmlAssets));
-// config.plugins = config.plugins.filter(item => !(item instanceof webpack.ProgressPlugin ));
 module.exports = config;
--- front/karma.conf.js
+++ front/karma.conf.js
@@ -3,37 +3,42 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 const webpackConfig = require('./build/webpack.test.config');
-const path = require('path');
+// const path = require('path');
 // const {
 //     getAllTestFiles,
 // } = require('./util');
 
 module.exports = function (config) {
-    config.set({
+    const configuration = {
         // 指定要运行测试的浏览器，可以指定多个。必须要安装对应的加载器(launcher)，karma 会在调起本地的浏览器。
         browsers: ['Chrome', ],
+        customLaunchers: {// travis环境浏览器启动配置
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox', ],
+            },
+        },
         // 指定要使用的测试框架
         frameworks: ['mocha', 'chai', ],
         client: {
             useIframe: false,
         },
         // 这个插件会将每个测试用例的测试结果打印到命令行 console 中。
-        reporters: ['spec', 'coverage-istanbul', ],
-        coverageIstanbulReporter: {
-            // 报告文件形式 参考: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
-            reports: ['html', 'lcovonly', 'text-summary', ],
-            // 报告打印位置
-            dir: path.join(__dirname, 'coverage'),
-
-            // 跳过未声明的文件
-            skipFilesWithNoCoverage: true,
-            'report-config': {
-              // 配置html报告位置
-              html: {
-                // outputs the report in ./coverage/html
-                subdir: 'html',
-              },
-            },
+        reporters: ['spec', 'coverage', ],
+        coverageReporter: {
+            // specify a common output directory
+            dir: './coverage',
+            reporters: [
+                // 生成 lcov.info 以及 html 文件，lcov.info 该文件中包含了详细的每个文件，每行，每个函数的执行信息。
+                {
+                    type: 'lcov',
+                    subdir: '.',
+                },
+                // 在命令行输出简要覆盖率数据
+                {
+                    type: 'text-summary',
+                },
+            ],
         },
         // 希望执行的测试文件, 这里的文件会经过 preprocessor 处理后，通过 script 便签添加到测试页面中。
         // 更多设置可以查看 https://karma-runner.github.io/2.0/config/files.html
@@ -45,7 +50,7 @@ module.exports = function (config) {
         }, ],
         // 使用 webapck 对文件进行编译打包，同时配置 sourcemap 方便调试代码
         preprocessors: {
-            'test/**/*.test.js': ['webpack', 'sourcemap', ],
+            'test/**/*.test.js': ['webpack', 'sourcemap', 'coverage', ],
         },
         // wepack 配置项
         webpack: webpackConfig,
@@ -58,9 +63,10 @@ module.exports = function (config) {
         },
         // 运行一次后退出，如果设为 true，运行后会默认 watch "files" 中指定的文件，如果有修改会自动重新执行。
         singleRun: true,
-        // 监听地址，默认为0.0.0.0
-        listenAddress: 'localhost',
-        customContextFile: './template/index.html',
-        processKillTimeout: 30 * 60 * 1000,
-    });
+    };
+    if (process.env.TRAVIS) {
+        // travis环境下使用对应的启动浏览器配置
+        configuration.browsers = ['Chrome_travis_ci', ];
+    }
+    config.set(configuration);
 };

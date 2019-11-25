c9b57945a2e5d11fd75c9f955622a67a1ecc3b02
Corleone <corleonexiao@qq.com>
2019-11-26 02:55:14 +0800

    新增footer组件

---
+++ front/src/app/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Layout, } from 'antd';
+import style from './index.css';
+
+const { Footer, } = Layout;
+export default () => (
+    <Footer className={style.footer}>
+        Corleone Blog @2019
+    </Footer>);

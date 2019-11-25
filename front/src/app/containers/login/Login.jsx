c6a876c5f155f8b7dfe3acf8e9311d206b84f8a1
Corleone <corleonexiao@qq.com>
2019-11-26 03:09:48 +0800

    修复了一个BUGG，删除了多余的代码

--- front/src/app/containers/login/Login.jsx
+++ front/src/app/containers/login/Login.jsx
@@ -2,21 +2,27 @@ import React, { PureComponent, } from 'react';
 import loadable from '@loadable/component';
 import { connect, } from 'react-redux';
 import { actions as loginActions, } from '@/reducers/loginReducer';
+import { actions as manageActions, } from '@/reducers/manageReducer';
+import { actions as frontActions, } from '@/reducers/frontReducer';
 
 const captchaUrl = 'http://localhost:2333/admin/captcha';
 const LoginForm = loadable(() => import('./LoginForm'));
 
 class LoginPage extends PureComponent {
-  constructor() {
-    super();
-    console.log('the LoginForm:', <LoginForm />);
-  }
   componentDidMount() {
     this.props.provide_api(this.afterLogin);
   }
   afterLogin = (userInfo) => {
-    sessionStorage.setItem('info', JSON.stringify(userInfo));
-    this.props.history.push('/');
+    if (userInfo.menus.length === 0) {
+      this.props.manage_error('该用户没有任何权限，无法登录，请联系管理员!');
+    } else {
+      if (typeof sessionStorage !== 'undefined') {
+        sessionStorage.setItem('info', JSON.stringify(userInfo));
+      }
+      this.props.history.push('/');
+      this.props.get_all_tags();
+      this.props.get_article_list();
+    }
   }
   extraH = '';
   handleSubmit = (event) => {
@@ -36,15 +42,15 @@ class LoginPage extends PureComponent {
     this.captchaUrl = captchaUrl + '?ts=' + Date.now();
     const isLogin = this.props.to === 'goto_signin';
     return (
-        <LoginForm
-          captchaUrl={this.captchaUrl}
-          capTs={this.props.par}
-          changeLoginStatus={this.changeLoginStatus}
-          handleSubmit={this.handleSubmit}
-          isLogin={isLogin}
-          returnHome={this.returnHome}
-          wrappedComponentRef={instance => this.loginForm = instance}
-        />
+      <LoginForm
+        captchaUrl={this.captchaUrl}
+        capTs={this.props.par}
+        changeLoginStatus={this.changeLoginStatus}
+        handleSubmit={this.handleSubmit}
+        isLogin={isLogin}
+        returnHome={this.returnHome}
+        wrappedComponentRef={instance => this.loginForm = instance}
+      />
     );
   }
 }
@@ -53,4 +59,4 @@ function mapStateToProps(state) {
     ...state.login,
   };
 }
-export default connect(mapStateToProps, loginActions)(LoginPage);
+export default connect(mapStateToProps, { ...loginActions, ...manageActions, ...frontActions, })(LoginPage);

7a84f657997368fa7b764b1a108ab1940623ef66
Corleone <corleonexiao@qq.com>
2019-11-26 02:44:53 +0800

    重命名的单元测试打包配置文件

---
+++ front/build/webpack.test.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const webpack = require('webpack');
+const AddHtmlAssets = require('add-asset-html-webpack-plugin');
+
+const config = merge(baseConfig, {
+    mode: 'development',
+    devtool:'eval',
+    plugins: [
+        new webpack.DefinePlugin({
+            'progress.env.BABEL_ENV': JSON.stringify('test'),
+        }),
+    ],
+});
+
+config.plugins = config.plugins.filter(item => !(item instanceof webpack.DllReferencePlugin || item instanceof AddHtmlAssets));// 去掉dll依赖，以为只有在index.html能访问dll对应的库
+module.exports = config;

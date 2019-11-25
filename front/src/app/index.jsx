a073d78c66dab5762346345a6531dd3a0a93b462
Corleone <corleonexiao@qq.com>
2019-11-26 03:44:37 +0800

    添加了SSR状态同步的校验，将render改为hydrate注水接口

--- front/src/app/index.jsx
+++ front/src/app/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import loadable from '@loadable/component';
-import { render, } from 'react-dom';
+import loadable, { loadableReady, } from '@loadable/component';
+import { hydrate, } from 'react-dom';
 import configureStore from './configureStore';
 import { Provider, } from 'react-redux';
+
 const root = document.getElementById('root');
-const store = configureStore();
+let initState = {};
+if (typeof window !== 'undefined' && window.ssrState) {
+  if (!ssrState.front.tags.user)
+    sessionStorage.clear();
+  initState = window.ssrState;
+}
+const store = configureStore(initState);
 const Index = loadable(() => import('./containers/IndexPage'));
-render((
-  <Provider store={store}>
-    <Index />
-  </Provider>), root);
+loadableReady(() => {
+  hydrate((
+    <Provider store={store}>
+      <Index />
+    </Provider>), root);
+});
 if (module.hot && process.env.NODE_ENV !== 'production') {
   module.hot.accept();
 }

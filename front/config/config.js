06d7f44b235cc104a34d75c97a11022a41e73649
Corleone <corleonexiao@qq.com>
2019-11-26 02:48:40 +0800

    ip，端口号的配置文件，根据情况自行修改

--- front/config/config.js
+++ front/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'localhost',
-    port: process.env.NODE_ENV === 'production' ? 8888 : 3288,
-    apiHost: '192.168.31.92',
-    apiPort: '2333',
+    port: process.env.NODE_ENV === 'production' ? 80 : 3566,// 端口号
+    apiHost: '192.168.101.102', //这里需要改成自己的服务器ip地址
+    apiPort: '2333', //服务器端口
 };
